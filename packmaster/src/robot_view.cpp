f5f498789d66e91da0b6b59195c10f6b73f89279
Agustin Espana <agustinespania@gmail.com>
2025-03-25 18:13:04 +0100

    feat: added case for unavailable distances

--- packmaster/src/robot_view.cpp
+++ packmaster/src/robot_view.cpp
@@ -5,7 +5,6 @@
 
 const double FOV_DEGREES = 30.0;
 const double DEG2RAD = M_PI / 180.0;
-const double MAX_DISTANCE = 166.5;
 
 RobotView::RobotView(Controller& controller)
     : controller(controller), window(nullptr), renderer(nullptr) {
@@ -36,11 +35,21 @@ void RobotView::handle_events() {
 
 void RobotView::draw_sensor_cone(float cx, float cy, double angle,
                                  double distance) {
-    if (distance == 0) {
-        distance = MAX_DISTANCE;
+    SDL_Color color;
+    if (distance == 0.0) {
+        color = {64, 64, 64, 255};  // Gray
+        distance = 220.0;
+    } else if (distance < 20) {
+        color = {255, 0, 0, 255};  // Red
+    } else if (distance < 40) {
+        color = {255, 165, 0, 255};  // Orange
+    } else if (distance < 60) {
+        color = {255, 255, 0, 255};  // Yellow
+    } else {
+        color = {0, 255, 0, 255};  // Green
     }
 
-    float scale_factor = 3.0f;
+    float scale_factor = 2.4f;
     distance *= scale_factor;
 
     double left_angle = (angle + FOV_DEGREES / 2.0) * DEG2RAD;
@@ -50,17 +59,6 @@ void RobotView::draw_sensor_cone(float cx, float cy, double angle,
     float y1 = cy + distance * sin(left_angle);
     float x2 = cx + distance * cos(right_angle);
     float y2 = cy + distance * sin(right_angle);
-
-    SDL_Color color;
-    if (distance < 20 * scale_factor) {
-        color = {255, 0, 0, 255};  // Red
-    } else if (distance < 40 * scale_factor) {
-        color = {255, 165, 0, 255};  // Orange
-    } else if (distance < 60 * scale_factor) {
-        color = {255, 255, 0, 255};  // Yellow
-    } else {
-        color = {0, 255, 0, 255};  // Green
-    }
 
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
     SDL_RenderLine(renderer, cx, cy, x1, y1);
