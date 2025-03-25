19444ea26651e05839ade1116f6a3ec77948bcfc
Agustin Espana <agustinespania@gmail.com>
2025-03-25 13:41:18 +0100

    feat: added color according to distance

--- packmaster/src/robot_view.cpp
+++ packmaster/src/robot_view.cpp
@@ -1,7 +1,7 @@
 #include "../include/robot_view.h"
 
-#include <mutex>
 #include <cmath>
+#include <mutex>
 
 const double FOV_DEGREES = 30.0;
 const double DEG2RAD = M_PI / 180.0;
@@ -34,7 +34,8 @@ void RobotView::handle_events() {
     }
 }
 
-void RobotView::draw_sensor_cone(float cx, float cy, double angle, double distance) {
+void RobotView::draw_sensor_cone(float cx, float cy, double angle,
+                                 double distance) {
     if (distance == 0) {
         distance = MAX_DISTANCE;
     }
@@ -47,12 +48,28 @@ void RobotView::draw_sensor_cone(float cx, float cy, double angle, double distan
     float x2 = cx + distance * cos(right_angle);
     float y2 = cy + distance * sin(right_angle);
 
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 100);
+    SDL_Color color;
+    if (distance < 20) {
+        color = {255, 0, 0, 255};  // Red
+    } else if (distance < 40) {
+        color = {255, 165, 0, 255};  // Orange
+    } else if (distance < 60) {
+        color = {255, 255, 0, 255};  // Yellow
+    } else {
+        color = {0, 255, 0, 255};  // Green
+    }
+    float r = color.r / 255.0f;
+    float g = color.g / 255.0f;
+    float b = color.b / 255.0f;
+    float a = color.a / 255.0f;
+
     SDL_Vertex v[3] = {
-        {cx, cy, {255, 0, 0, 100}},
-        {x1, y1, {255, 0, 0, 100}},
-        {x2, y2, {255, 0, 0, 100}}
+        {cx, cy, {r, g, b, a}},  // Central vertex (robot direction)
+        {x1, y1, {r, g, b, a}},  // Left vertex
+        {x2, y2, {r, g, b, a}}   // Right vertex
     };
+
+    // Draw the sensor cone using geometry
     SDL_RenderGeometry(renderer, NULL, v, 3, NULL, 0);
 }
 
@@ -68,7 +85,8 @@ void RobotView::render() {
     float robot_x = windowWidth / 2.0f;
     float robot_y = windowHeight - 80.f;
     float robot_size = 40.0f;
-    SDL_FRect robot_rect = {robot_x - robot_size / 2, robot_y - robot_size / 2, robot_size, robot_size};
+    SDL_FRect robot_rect = {robot_x - robot_size / 2, robot_y - robot_size / 2,
+                            robot_size, robot_size};
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderRect(renderer, &robot_rect);
 
